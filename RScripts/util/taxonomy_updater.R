727e6bbd51f6d786a33b6b8c13f3a5c70c68382a
seninp <seninp@gmail.com>
2015-12-16 17:58:18 +0100

    taonomy updater adde

--- RScripts/util/taxonomy_updater.R
+++ RScripts/util/taxonomy_updater.R
@@ -5,7 +5,14 @@ require(dplyr)
 require(stringr)
 require(data.table)
 #
-hits <- fread("../PerlCode/test/test.txt", sep = "\t")
+# load the data
+#
+hits <- fread("../PerlCode/test/table.txt", sep = "\t")
+setnames(hits, c("query_name","hit_name","description","identity","bits","query_length",
+                 "alignment_length","gaps","evalue"))
+head(hits, 3)
+#
+# define the query function
 #
 organism_for_id <- function(id) {
   r_search <- entrez_search(db = "nuccore", term = id)
@@ -15,9 +22,9 @@ organism_for_id <- function(id) {
   org
 }
 #
+# run the update
 #
-#
-organisms <- adply(hits$V2, 1, function(x){
+organisms <- adply(hits$hit_name, 1, function(x){
   t <- str_match(x, "(?<=\\|)(.*?)(?=\\|)")[1,2]
   if (!any(is.na(t))) {
     res <- try(organism_for_id(t),silent = TRUE)
@@ -30,3 +37,12 @@ organisms <- adply(hits$V2, 1, function(x){
   c(x, NA)
 }, .progress = progress_text(char = ":"))
 #
+# merg the retrieved data with the table
+#
+organisms = select(organisms, c(2, 3))
+setnames(organisms, c("hit_name","organism"))
+hits = merge(hits, organisms, by = c("hit_name"))
+#
+# save the results
+#
+write.table(hits, "hits_with_affiliation.txt", sep = "\t", col.names = T,row.names = F, quote=T)

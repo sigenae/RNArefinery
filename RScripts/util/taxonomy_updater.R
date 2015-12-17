d4954c265827ebc9cdeb95f78f95d45201b3012d
seninp <seninp@gmail.com>
2015-12-17 10:33:18 +0100

    updating the taxonomy retrieval with extra steps avoiding the failure

--- RScripts/util/taxonomy_updater.R
+++ RScripts/util/taxonomy_updater.R
@@ -7,27 +7,45 @@ require(data.table)
 #
 # load the data
 #
-hits <- fread("../PerlCode/test/table.txt", sep = "\t")
+options(echo = TRUE) # if you want see commands in output file
+args <- commandArgs(trailingOnly = TRUE)
+print(paste("arguments supplied:",args))
+#
+# [0.1] take care about the input file names
+hits <- args[1]
+database_name <- args[2]
+out_hits <- args[3]
+#
+# [0.2] read the input
+#
+hits <- fread(hits, sep = "\t")
 setnames(hits, c("query_name","hit_name","description","identity","bits","query_length",
                  "alignment_length","gaps","evalue"))
 head(hits, 3)
 #
-# define the query function
+# [0.3] define the query function
 #
 organism_for_id <- function(id) {
-  r_search <- entrez_search(db = "nuccore", term = id)
-  org <- entrez_summary(db = "nuccore", id = r_search$ids)$organism
+  r_search <- entrez_search(db = database_name, term = id)
+  org <- entrez_summary(db = database_name, id = r_search$ids)$organism
   # NCBI bans IPs which issue more than 3 requests per second
-  Sys.sleep(0.5)
+  Sys.sleep(0.33)
   org
 }
 #
-# run the update
+# [0.4] run the update
 #
-organisms <- adply(hits$hit_name, 1, function(x){
+# Caveat:
+# because of this
+# "Erreur dans list_to_dataframe(res, attr(.data, "split_labels"), .id, id_as_factor) : 
+# Results do not have equal lengths"
+# 
+# we shall use lists
+#
+organisms <- alply(unique(hits$hit_name), 1, function(x){
   t <- str_match(x, "(?<=\\|)(.*?)(?=\\|)")[1,2]
   if (!any(is.na(t))) {
-    res <- try(organism_for_id(t),silent = TRUE)
+    res <- try(organism_for_id(t), silent = TRUE)
     if (inherits(res,'try-error')) {
       return(c(x, NA))
     } else {
@@ -36,13 +54,14 @@ organisms <- adply(hits$hit_name, 1, function(x){
   }
   c(x, NA)
 }, .progress = progress_text(char = ":"))
+org_table <- ldply(organisms, function(x){data.frame(hit_id = x[1], organism = x[2], 
+                                                     stringsAsFactors = F)})[,2:3]
+row.names(org_table) <- org_table$hit_id
 #
-# merg the retrieved data with the table
+# [0.5] merge the retrieved data with the table
 #
-organisms = select(organisms, c(2, 3))
-setnames(organisms, c("hit_name","organism"))
-hits = merge(hits, organisms, by = c("hit_name"))
+hits$organism <- daply(hits, .(query_name), function(x){org_table[paste(x$hit_name),]$organism})
 #
-# save the results
+# [0.6] save the results
 #
-write.table(hits, "hits_with_affiliation.txt", sep = "\t", col.names = T,row.names = F, quote=T)
+write.table(hits, out_hits, sep = "\t", col.names = T, row.names = F, quote = T)

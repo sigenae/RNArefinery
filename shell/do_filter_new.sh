121629d1106e6d06420c1ffb16fe7a5d133b5427
Pavel Senin <seninp@gmail.com>
2016-01-31 17:47:38 -0700

    aligning to prots

--- shell/do_filter_new.sh
+++ shell/do_filter_new.sh
@@ -42,15 +42,28 @@ echo "  making raw data and llist..." >&2
 cat ${SEARCHPATH}/transcripts_fpkm_3.fa | sed "s/>/>${SEARCHPATH}_/g" >${SEARCHPATH}/filters/raw_contigs.fa
 makellist ${SEARCHPATH}/filters/raw_contigs.fa >${SEARCHPATH}/filters/raw_contigs.llist
 #
-# 0.3. run blat using the data
+# 0.4. run blat using the MRNA5 data
 #
-echo "  running BLAT on NEW MRNA SET..." >&2
-blat ${reference5_mrna} ${SEARCHPATH}/filters/raw_contigs.fa ${SEARCHPATH}/filters/blat_raw_genome5_mrna.psl
-cat ${SEARCHPATH}/filters/blat_raw_genome5_mrna.psl | ${refinery_blatparser} > ${SEARCHPATH}/filters/blat_raw_genome5_mrna.best.tsv
+echo "  running BLAT on the NEW CDNA/MRNA? SET..." >&2
+blat ${reference5_mrna} ${SEARCHPATH}/filters/raw_contigs.fa ${SEARCHPATH}/filters/blat_cdna5.psl
+cat ${SEARCHPATH}/filters/blat_cdna5.psl | ${refinery_blatparser} > ${SEARCHPATH}/filters/blat_cdna5.best.tsv
 #
-# 0.4 filter those with less than 20% overlap on CDS
+# 0.5 filter those with less than 20% overlap on MRNA template
 #
 echo "  filtering raw contigs using NEW MRNA SET alignment..." >&2
-Rscript ${refinery_bin}/filter_square75.R ${SEARCHPATH}/filters/raw_contigs.llist ${SEARCHPATH}/filters/blat_raw_genome5_mrna.best.tsv ${SEARCHPATH}/filters/genome5_mrna_keepers.list
-include_mf ${SEARCHPATH}/filters/raw_contigs.fa ${SEARCHPATH}/filters/contigs_after_genome5_mrna.fa ${SEARCHPATH}/filters/genome5_mrna_keepers.list
-makellist ${SEARCHPATH}/filters/contigs_after_genome5_mrna.fa >${SEARCHPATH}/filters/contigs_after_genome5_mrna.llist
+Rscript ${refinery_bin}/filter_square75.R ${SEARCHPATH}/filters/raw_contigs.llist ${SEARCHPATH}/filters/blat_cdna5.best.tsv ${SEARCHPATH}/filters/blat_cdna5.keepers.list
+include_mf ${SEARCHPATH}/filters/raw_contigs.fa ${SEARCHPATH}/filters/contigs_after_cdna5.fa ${SEARCHPATH}/filters/blat_cdna5.keepers.list
+makellist ${SEARCHPATH}/filters/contigs_after_cdna5.fa >${SEARCHPATH}/filters/contigs_after_cdna5.llist
+#
+# 0.6. run blat using the PEP5 data
+#
+echo "  running BLAT on NEW PEPTIDE SET..." >&2
+blat -t=dnax ${SEARCHPATH}/filters/contigs_after_cdna5.fa -q=prot ${reference5_protein} ${SEARCHPATH}/filters/blat_cdna5_pep5.psl
+cat ${SEARCHPATH}/filters/blat_cdna5_pep5.psl | ${refinery_blatparser} -f t > ${SEARCHPATH}/filters/blat_cdna5_pep5.best.tsv
+#
+# 0.7. re-filter by the REFSEQ PEP data
+#
+echo "  filtering contigs using BLAT REFSEQ PEP alignment..." >&2
+Rscript ${refinery_bin}/filter_square75_iverse.R ${SEARCHPATH}/filters/contigs_after_mrna5.llist ${SEARCHPATH}/filters/blat_cdna5_pep5.best.tsv ${SEARCHPATH}/filters/blat_cdna5_pep5_keepers.list
+include_mf ${SEARCHPATH}/filters/raw_contigs.fa ${SEARCHPATH}/filters/contigs_after_cdna5_pep5.fa ${SEARCHPATH}/filters/blat_cdna5_pep5_keepers.list
+makellist ${SEARCHPATH}/filters/contigs_after_cdna5_pep5.fa >${SEARCHPATH}/filters/contigs_after_cdna5_pep5.llist
